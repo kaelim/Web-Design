9f70ee618312ce11eb8a3aa1d493b4023e18b254
Kaeli Moore <143522412+kaelim@users.noreply.github.com>
2024-05-05 20:21:54 -0600

    Add files via upload

---
+++ Assignment14/js/main.js
@@ -0,0 +1,11 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "30px Arial";
+ctx.strokeText('Nintendo',400,300);
+ctx.strokeText('Ubisoft',100,50);
+ctx.strokeText('Iron Galaxy',800,500);
+ctx.strokeText('2K',800,100);
+ctx.strokeText('Epic Games',1000,370);
+ctx.strokeText('Bungie',40,700);
+ctx.strokeText('T2',500,900);
+ctx.strokeText('Gameloft',1000,1000);
